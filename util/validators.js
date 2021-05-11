2fe45c802a57fa3bb822620ef7fdc53194a70e76
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-05-11 12:02:45 +0200

    login with validations

--- util/validators.js
+++ util/validators.js
@@ -9,7 +9,7 @@ module.exports.validateRegisterInput = (
         errors.username = 'Username must not be empty'
     }
     if (email.trim() === '') {
-        errors.email = 'Username must not be empty'
+        errors.email = 'Email must not be empty'
     } else {
         const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
         if (!email.match(regEx)) {
@@ -20,6 +20,21 @@ module.exports.validateRegisterInput = (
         errors.password = 'Password must not be empty'
     } else if (password != confirmPassword) {
         errors.confirmPassword = 'Passwords must match'
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
+
+module.exports.validateLoginInput = (username, password) => {
+    const errors = {}
+    if (username.trim() === '') {
+        errors.username = 'Username must not be empty'
+    }
+    if (password.trim() === '') {
+        errors.username = 'Password must not be empty'
     }
 
     return {
