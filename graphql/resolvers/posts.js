d365b5fa5768a02ef25aae021aa82ab65d675269
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-05-11 18:46:28 +0200

    fix buffering mongoose bug

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,40 +1,37 @@
-const { AuthenticationError, UserInputError } = require('apollo-server')
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
-const Post = require('../../models/Post')
-const checkAuth = require('../../util/check-auth')
+const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
-//fonctions qui vont s'executer quand on va lancer la query correspondante
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                // Post est ce qu'on a require plus haut
-                //sort by createdAt desc
-                const posts = await Post.find().sort({ createdAt: -1 })
-                return posts
+                const posts = await Post.find().sort({ createdAt: -1 });
+                return posts;
             } catch (err) {
-                throw new Error(err)
+                throw new Error(err);
             }
         },
         async getPost(_, { postId }) {
             try {
-                const post = await Post.findById(postId)
+                const post = await Post.findById(postId);
                 if (post) {
-                    return post
+                    return post;
                 } else {
-                    throw new Error('Post not found')
+                    throw new Error('Post not found');
                 }
             } catch (err) {
-                throw new Error(err)
+                throw new Error(err);
             }
         }
     },
     Mutation: {
         async createPost(_, { body }, context) {
-            const user = checkAuth(context)
+            const user = checkAuth(context);
 
-            if (args.body.trim() === '') {
-                throw new Error('Post body must not be empty')
+            if (body.trim() === '') {
+                throw new Error('Post body must not be empty');
             }
 
             const newPost = new Post({
@@ -42,51 +39,50 @@ module.exports = {
                 user: user.id,
                 username: user.username,
                 createdAt: new Date().toISOString()
-            })
+            });
 
-            const post = await newPost.save()
+            const post = await newPost.save();
 
             context.pubsub.publish('NEW_POST', {
                 newPost: post
-            })
+            });
 
-            return post
+            return post;
         },
         async deletePost(_, { postId }, context) {
-            const user = checkAuth(context)
-            console.log(user)
+            const user = checkAuth(context);
 
             try {
-                const post = await Post.findById(postId)
+                const post = await Post.findById(postId);
                 if (user.username === post.username) {
-                    await post.delete()
-                    return 'Post deleted successfully'
+                    await post.delete();
+                    return 'Post deleted successfully';
                 } else {
-                    throw new AuthenticationError('Action not allowed')
+                    throw new AuthenticationError('Action not allowed');
                 }
             } catch (err) {
-                throw new Error(err)
+                throw new Error(err);
             }
         },
         async likePost(_, { postId }, context) {
-            const { username } = checkAuth(context)
+            const { username } = checkAuth(context);
 
-            const post = await Post.findById(postId)
+            const post = await Post.findById(postId);
             if (post) {
-                if (post.likes.find(like => like.username === username)) {
-                    //Post already like, unlike it
-                    post.likes = post.likes.filter(like => like.username !== username)
+                if (post.likes.find((like) => like.username === username)) {
+                    // Post already likes, unlike it
+                    post.likes = post.likes.filter((like) => like.username !== username);
                 } else {
-                    //Post ,not like, like it
+                    // Not liked, like post
                     post.likes.push({
                         username,
                         createdAt: new Date().toISOString()
-                    })
+                    });
                 }
 
-                await post.save()
-                return post
-            } else throw new UserInputError('Post not found')
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
         }
     },
     Subscription: {
@@ -94,4 +90,4 @@ module.exports = {
             subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
-}
+};
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const { ApolloServer, PubSub } = require('apollo-server')
-const mongoose = require('mongoose')
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
 
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
-const { MONGODB } = require('./config.js')
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
 
-const pubsub = new PubSub()
+const pubsub = new PubSub();
 
-//creation du serveur Apollo avec les query et fonctions que l'ont a créé
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({
-        req,
-        pubsub
-    })
-})
+    context: ({ req }) => ({ req, pubsub })
+});
 
-//connexion a la base de donné mongoose
-mongoose.connect(MONGODB, { useNewUrlParser: true })
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
-        console.log('MONGODB connected')
-        //start le serveur Apollo (il fonctionne avec express)
-        return server.listen({ port: 5000 })
+        console.log('MongoDB Connected');
+        return server.listen({ port: PORT });
     })
-    .then(res => {
-        console.log(`server running at ${res.url}`)
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.error(err)
     })
