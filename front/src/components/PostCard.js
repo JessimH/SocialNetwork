28dec2ff80afe7f046374c57e94182c9fd5ea1d7
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-05-12 16:46:47 +0200

    endproject

--- front/src/components/PostCard.js
+++ front/src/components/PostCard.js
@@ -1,50 +1,48 @@
-import React from 'react'
-import { Card, Icon, Label, Image, Button } from 'semantic-ui-react'
-import moment from 'moment'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
 
-    function likePost() {
-        console.log('Like post!!')
-    }
-    function commentOnPost() {
-        console.log('comment on post!!!')
-    }
-    return (
-        <Card fluid>
-            <Card.Content>
-                <Image
-                    floated='right'
-                    size='mini'
-                    src='https://www.justifit.fr/wp-content/uploads/2020/06/Droit-a-limage.jpg'
-                />
-                <Card.Header>{username}</Card.Header>
-                <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
-                <Card.Description>
-                    {body}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button as='div' labelPosition='right' onClick={likePost}>
-                    <Button color='teal' basic>
-                        <Icon name='heart' />
-                    </Button>
-                    <Label basic color='teal' pointing='left'>
-                        {likeCount}
-                    </Label>
-                </Button>
-                <Button as='div' labelPosition='right' onClick={commentOnPost}>
-                    <Button color='blue' basic>
-                        <Icon name='comments' />
-                    </Button>
-                    <Label basic color='blue' pointing='left'>
-                        {commentCount}
-                    </Label>
-                </Button>
-            </Card.Content>
-        </Card>
-    )
+function PostCard({
+  post: { body, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <MyPopup content="Comment on post">
+          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+            <Button color="blue" basic>
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
+          </Button>
+        </MyPopup>
+        {user && user.username === username && <DeleteButton postId={id} />}
+      </Card.Content>
+    </Card>
+  );
 }
 
-export default PostCard
+export default PostCard;
